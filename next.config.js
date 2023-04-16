7f53b2854382486b2037c1e43821f79aa23f2b4f
Alex MacCaw <alex@alexmaccaw.com>
2023-04-16 17:10:55 -0400

    add pg-native

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
 
   experimental: {
     appDir: true,
-    serverComponentsExternalPackages: ['shiki', 'vscode-oniguruma', 'pg'],
+    serverComponentsExternalPackages: ['shiki', 'vscode-oniguruma', 'pg', 'pg-native'],
   },
 
   async rewrites() {
