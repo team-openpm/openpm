90972bb299ed920a55f6b6f15de65d0340aee82e
Alex MacCaw <alex@alexmaccaw.com>
2023-04-16 18:00:07 -0400

    assert DATABASE_URL

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -3,7 +3,11 @@ import 'server-only'
 import {Pool} from '@neondatabase/serverless'
 import {Kysely, PostgresDialect} from 'kysely'
 
+import {assertString} from '@/lib/assert'
+
 import {DB} from './types'
+
+assertString(process.env.DATABASE_URL, 'DATABASE_URL is not set')
 
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
