3d6ab94414614bc900221f06874d5441adc45f83
Alex MacCaw <alex@alexmaccaw.com>
2023-04-16 23:32:58 -0400

    moar

--- server/db/packages/types.ts
+++ server/db/packages/types.ts
@@ -21,12 +21,4 @@ export interface Package {
 export type PackageFull = Omit<Package, 'acl_write'>
 
 // Everything in Package except openapi and acl_write
-// Convert Date to string for JSON serialization
-export type PackageLite = Omit<
-  Package,
-  'openapi' | 'acl_write' | 'created_at' | 'updated_at' | 'published_at'
-> & {
-  created_at: string
-  updated_at: string
-  published_at: string
-}
+export type PackageLite = Omit<Package, 'openapi' | 'acl_write'>
