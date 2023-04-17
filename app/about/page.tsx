6a4e1ade594494040a6b4b4752debee4dc401c2f
Alex MacCaw <alex@alexmaccaw.com>
2023-04-16 21:50:15 -0400

    tweak

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,34 +15,30 @@ export default async function AboutPage() {
           </p>
 
           <p>
-            With mobile we learned the painful lesson of the Apple app store, controlled
-            by any single monopolistic company, and stifling innovation and
+            With mobile we learned the painful lesson of the Apple app-store, controlled
+            by a single monopolistic company, stifling innovation and
             entrepreneurship.
           </p>
 
           <p>
             AI, our new platform, needs it&apos;s own app-store. One not controlled by a
-            single rent-seeking company, but an open and free app-store.
+            single rent-seeking company, but an open and free app-store built upon the
+            open web and the OpenAPI specification. This will let AIs discover and
+            interact with the world via APIs.
           </p>
 
           <p>
-            We engineers currently have a slim chance of creating our own app-store before
-            some large corporation does it.
+            We engineers currently have a slim chance of creating this app-store layer
+            before some large corporation does it.
           </p>
 
           <p>We must seize this chance.</p>
 
           <p>
-            APIs are the apps on the AI platform. We need a headless app-store so that AIs
-            can discover and use APIs. A store that is built upon the open web and the
-            OpenAPI specification.
-          </p>
-
-          <p>
             <Link prefetch={false} href="/" className="text-pink-500">
               openpm.ai
             </Link>{' '}
-            is that store. We are an open source package-manager for OpenAPI files.
+            is that store. We are building an open source package-manager for OpenAPI files.
             Everything we release is under the MIT license. We will never charge a
             transaction fee for our services. We will never wield editorial control. We
             will only take down packages that are scams or illegal under US law. At any
