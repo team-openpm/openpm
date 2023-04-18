ccc45e0eb96b0bdc20ede86ea5aef807abbd19d7
Alex MacCaw <alex@alexmaccaw.com>
2023-04-17 23:52:29 -0400

    Revert "Revert "use api endpoint for searching""
    This reverts commit 7622092e22dca6f3b8a5849fb2553d5c48c66e0b.

--- app/packages/pagination.tsx
+++ app/packages/pagination.tsx
@@ -5,12 +5,12 @@ export function Pagination({
   page,
   limit,
   total,
-  query,
+  onChange,
 }: {
   page: number
   limit: number
   total: number
-  query: string
+  onChange: (page: number) => void
 }) {
   const pages = Math.ceil(total / limit)
   const pagesArray = Array.from({length: pages}, (_, i) => i + 1)
@@ -19,16 +19,19 @@ export function Pagination({
     <div className="mt-10 flex justify-center">
       <div className="flex space-x-2">
         {pagesArray.map((p) => (
-          <a
+          <button
             key={p}
-            href={`/apis?p=${p}&q=${query}`}
+            onClick={() => onChange(p)}
+            type="button"
             className={clsx(
               `rounded-md border border-pink-500/30 px-4 py-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2`,
-              p === page ? 'bg-pink-500 text-white hover:bg-pink-500/90' : 'bg-white text-pink-500 hover:bg-pink-500/20',
+              p === page
+                ? 'bg-pink-500 text-white hover:bg-pink-500/90'
+                : 'bg-white text-pink-500 hover:bg-pink-500/20',
             )}
           >
             {p}
-          </a>
+          </button>
         ))}
       </div>
     </div>
