493fffe85711562dbb5c91c6070773332232e908
Alex MacCaw <alex@alexmaccaw.com>
2023-04-18 10:30:54 -0400

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default async function AboutPage() {
   return (
     <MainTemplate>
       <article className="prose prose-slate dark:prose-invert prose-headings:text-xl prose-headings:font-medium prose-a:no-underline dark:prose-p:text-white/70 dark:prose-a:text-blue-500">
-        <h1>About</h1>
+        <h1>openpm</h1>
 
         <div className="prose max-w-prose">
           <p>
@@ -28,20 +28,22 @@ export default async function AboutPage() {
 
           <p>
             We engineers currently have a slim chance of creating this app-store layer
-            before some large corporation does it.
+            before some large corporation does it. We must seize this chance.
           </p>
 
-          <p>We must seize this chance.</p>
-
           <p>
+            That&apos;s why we&apos;re building{' '}
             <Link prefetch={false} href="/" className="text-pink-500">
               openpm.ai
-            </Link>{' '}
-            is that store. We are building an open source package-manager for OpenAPI
-            files. Everything we release is under the MIT license. We will never charge a
+            </Link>
+            , an open source package-manager for OpenAPI files.
+          </p>
+
+          <p>
+            Everything we release is under the MIT license. We will never charge a
             transaction fee for our services. We will never wield editorial control. We
-            will only take down packages that are scams or illegal under US law. At any
-            point you can choose to{' '}
+            will only remove packages that are scams or illegal under US law. At any point
+            you can choose to{' '}
             <Link prefetch={false} href="/export">
               export all of our packages
             </Link>{' '}
