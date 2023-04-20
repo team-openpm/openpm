ff06c73ae8085da989ceb276d5ffbda9d767010b
Alex MacCaw <alex@alexmaccaw.com>
2023-04-20 15:24:16 -0400

    example showing langchain

--- docs/examples/langchain.ts
+++ docs/examples/langchain.ts
@@ -1,16 +1,14 @@
 import {initializeAgentExecutorWithOptions} from 'langchain/agents'
-import {OpenAI} from 'langchain/llms/openai'
-import {SerpAPI, Tool} from 'langchain/tools'
-import {Calculator} from 'langchain/tools/calculator'
 import {ChatOpenAI} from 'langchain/chat_models/openai'
-import {initializeAgentExecutorWithOptions} from 'langchain/agents'
-import {RequestsGetTool, RequestsPostTool, AIPluginTool} from 'langchain/tools'
+import {RequestsGetTool, RequestsPostTool} from 'langchain/tools'
+
+import {OpenpmTool} from './openpm-tool'
 
 async function main() {
   const tools = [
     new RequestsGetTool(),
     new RequestsPostTool(),
-    await AIPluginTool.fromPluginUrl('https://www.klarna.com/.well-known/ai-plugin.json'),
+    await OpenpmTool.fromPackageId('klarna'),
   ]
   const agent = await initializeAgentExecutorWithOptions(
     tools,
@@ -26,66 +24,3 @@ async function main() {
 }
 
 main()
-
-export interface AIPluginToolParams {
-  name: string
-  description: string
-  apiSpec: string
-}
-
-export class OpenpmTool extends Tool implements AIPluginToolParams {
-  private _name: string
-
-  private _description: string
-
-  apiSpec: string
-
-  get name() {
-    return this._name
-  }
-
-  get description() {
-    return this._description
-  }
-
-  constructor(params: AIPluginToolParams) {
-    super()
-    this._name = params.name
-    this._description = params.description
-    this.apiSpec = params.apiSpec
-  }
-
-  /** @ignore */
-  async _call(_input: string) {
-    return this.apiSpec
-  }
-
-  static async fromPackageIds(packageIds: string[]) {
-    const aiPluginRes = await fetch(
-      'https://openpm.ai/api/packages/lookup?ids=' + packageIds.join(','),
-    )
-
-    if (!aiPluginRes.ok) {
-      throw new Error(
-        `Failed to fetch plugin from ${url} with status ${aiPluginRes.status}`,
-      )
-    }
-    const aiPluginJson = await aiPluginRes.json()
-
-    const apiUrlRes = await fetch(aiPluginJson.api.url)
-    if (!apiUrlRes.ok) {
-      throw new Error(
-        `Failed to fetch API spec from ${aiPluginJson.api.url} with status ${apiUrlRes.status}`,
-      )
-    }
-    const apiUrlJson = await apiUrlRes.text()
-
-    return new AIPluginTool({
-      name: aiPluginJson.name_for_model,
-      description: `Call this tool to get the OpenAPI spec (and usage guide) for interacting with the ${aiPluginJson.name_for_human} API. You should only call this ONCE! What is the ${aiPluginJson.name_for_human} API useful for? ${aiPluginJson.description_for_human}`,
-      apiSpec: `Usage Guide: ${aiPluginJson.description_for_model}
-
-OpenAPI Spec: ${apiUrlJson}`,
-    })
-  }
-}
