e2b43f162a885c6e43475d9efa6d45d0d9fff624
Alex MacCaw <alex@alexmaccaw.com>
2023-04-19 21:29:04 -0400

    Some packages are quite large!

--- app/packages/[packageId]/page.tsx
+++ app/packages/[packageId]/page.tsx
@@ -1,7 +1,9 @@
 import truncate from 'lodash/truncate'
+import Link from 'next/link'
 import React from 'react'
 
 import {AccountHeader} from '@/components/account-header'
+import {MainTemplate} from '@/components/main-template'
 import {PackageMain} from '@/components/show-package/package-main'
 import {PackageSidebar} from '@/components/show-package/package-sidebar'
 import {parseOpenApiSpecJson} from '@/lib/openapi'
@@ -41,6 +43,11 @@ export async function generateMetadata({params}: {params: {packageId: string}}) 
 
 export default async function Package({params}: {params: {packageId: string}}) {
   const pkg = await getPackageByIdOrNotFound(params.packageId)
+
+  if (pkg.openapi.length > MAX_PACKAGE_SIZE) {
+    return <TooLargeToRender packageId={pkg.id} />
+  }
+
   const doc = await parseOpenApiSpecJson(pkg.openapi)
 
   return (
@@ -55,5 +62,24 @@ export default async function Package({params}: {params: {packageId: string}}) {
         <PackageMain package={pkg} document={doc} />
       </div>
     </div>
+  )
+}
+
+// Can't be larger than 300kb
+const MAX_PACKAGE_SIZE = 300 * 1024
+
+function TooLargeToRender({packageId}: {packageId: string}) {
+  return (
+    <MainTemplate>
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <h2 className="text-xl">Package too large to render</h2>
+
+        <h3>
+          <Link className="text-blue-500" href={`/api/packages/${packageId}/openapi`}>
+            See full OpenAPI spec
+          </Link>
+        </h3>
+      </div>
+    </MainTemplate>
   )
 }
